4c7866530be0372502dda8a4b24525e3154d08e6
Subin Shakya <subin12.shakya@gmail.com>
2025-04-21 16:51:38 +0545

    add random email for sign up and headless driver

--- conftest.py
+++ conftest.py
@@ -1,14 +1,21 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from pages.signup_page import SignupPage
 
 
 @pytest.fixture
 def setup():
-    driver = webdriver.Chrome()
-    driver.get(
-        "https://ns-code-snippet-9eae23357ebe.herokuapp.com/"
-    )  
+    options = Options()
+    options.add_argument("--headless")  # Run Chrome in headless mode
+    options.add_argument("--disable-gpu")  # Disable GPU acceleration
+    options.add_argument("--window-size=1920,1080")  # Optional, but useful
+    options.add_argument("--no-sandbox")  # Good for CI environments
+    options.add_argument("--disable-dev-shm-usage")  # Prevent resource issues
+
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://ns-code-snippet-9eae23357ebe.herokuapp.com/")
     signup_page = SignupPage(driver)
+
     yield driver, signup_page
     driver.quit()
--- tests/test_signup.py
+++ tests/test_signup.py
@@ -1,14 +1,15 @@
 import pytest
 from selenium.common.exceptions import TimeoutException
 from locators.signup_locators import SignupLocators
+import uuid
 
 
 class TestSignup:
     def test_valid_signup(self, setup):
         driver, signup_page = setup
-
+        random_email = f"user_{uuid.uuid4().hex[:8]}@example.com"
         signup_page.click_signup_btn()
-        signup_page.enter_email("useradcdme@gmail.com")
+        signup_page.enter_email(random_email)
         signup_page.enter_password("password123")
         signup_page.enter_confirm_password("password123")
         signup_page.click_signup_submit_button()
